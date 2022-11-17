b3c031815e6cb494dc84b91531b9b59376dadf82
Kim gimin <ggm4954@gmail.com>
2022-11-17 09:31:07 +0900

    gimin imageCommentModify

--- project/app.py
+++ project/app.py
@@ -1,9 +1,43 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+client = MongoClient('mongodb+srv://text:sparta@cluster0.qytxmse.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
 @app.route('/')
 def home():
    return render_template('index.html')
 
+@app.route("/project", methods=["POST"])
+def project_post():
+   name_receive = request.form['name_give']
+   comment_receive = request.form['comment_give']
+
+   doc = {
+      'name':name_receive,
+      'comment':comment_receive
+   }
+
+   db.project.insert_one(doc)
+
+
+   return jsonify({'msg': '응원 완료!'})
+
+@app.route("/project", methods=["GET"])
+def project_get():
+   comment_list = list(db.project.find({}, {'_id': False}))
+   return jsonify({'comments': comment_list})
+
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
+
+
+
+
+
+
+
+
