05b9b69bd2439672a555b00394b43b0153d7b158
St0ne96 <bunsuk1637@gmail.com>
2022-11-17 19:22:25 +0900

    파일옮김

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://test:sparta@cluster0.a7l8csh.mongodb.net/?retryWrites=true&w=majority")
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+    return render_template('teamtest.html')
+
+@app.route('/templates')
+def templates():
+    return render_template('intro.html')
+
+
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form["name_give"]
+    comment_receive = request.form["comment_give"]
+
+    comment_list = list(db.homework.find({}, {'_id': False}))
+    count = len(comment_list) + 1
+
+    doc = {
+        'num': count,
+        'name': name_receive,
+        'comment': comment_receive,
+        'done': 0
+    }
+
+    db.homework.insert_one(doc)
+    return jsonify({'msg': '작성완료'})
+
+
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    comment_list = list(db.homework.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
+
+
+@app.route('/homework/delete', methods=['POST'])
+def homework_delete():
+    num_receive = request.form['num_give']
+    db.homework.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
+    return jsonify({'msg': '삭제 완료!'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5555, debug=True)
